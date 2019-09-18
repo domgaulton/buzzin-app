90a103837d391268dfff61bd3a85efef42cc1fc8
Dom Gaulton <d.gaulton@catchdigital.com>
2019-09-18 18:12:47 +0100

    BZ-9 tavern pin attempts

--- src/components/Tavern.js
+++ src/components/Tavern.js
@@ -3,17 +3,27 @@ import { firestore } from "../base";
 
 function Tavern(props) {
 
+  let members = [];
+
+
   const [tavernName, setTavernName] = useState('');
   const [countdown, setCountdown] = useState(undefined);
   const [membersReady, setMembersReady] = useState(false);
   const [memberSet, setMembers] = useState([]);
 
-  // - WORKING - https://firebase.google.com/docs/firestore/quickstart
-  firestore.collection("taverns").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-      console.log(`${doc.id} => ${doc.data()}`);
-    });
+  firestore.collection("taverns").doc(props.match.params.tavernId).get().then((querySnapshot) => {
+    console.log(querySnapshot.data().options.pin)
   });
+
+  // const pin = () => {
+  //   firestore.collection("taverns").doc(props.match.params.tavernId).get().then((querySnapshot) => {
+  //     return querySnapshot.data().options.pin;
+  //   });
+  // }
+
+  const pin = () => {
+    firestore.collection("taverns").doc(props.match.params.tavernId).get().then((querySnapshot) => querySnapshot.data().options.pin);
+  }
 
   // https://firebase.google.com/docs/firestore/query-data/listen
   firestore.collection("taverns").doc(props.match.params.tavernId)
@@ -22,13 +32,16 @@ function Tavern(props) {
       includeMetadataChanges: true
     }, function(doc) {
       const source = doc.metadata.hasPendingWrites ? "Local" : "Server";
-      console.log(source, " data: ", doc.data());
+      // console.log(source, " data: ", doc.data());
+
+      const pin = doc.data().options.pin;
 
       const tavernName = doc.data().name;
-      console.log(tavernName);
+      // console.log(tavernName);
       setTavernName(tavernName);
 
-      const members = doc.data().members;
+      members = doc.data().members;
+      console.log(members)
       const membersReady = members.every(item => {
         return item.ready === true;
       })
@@ -44,6 +57,7 @@ function Tavern(props) {
       <h1>{tavernName}</h1>
       <p>Time limit: {countdown}</p>
       <p>Members are {!membersReady ? 'not' : ''} ready!</p>
+      <p>Pin: {pin()}</p>
     </div>
   );
 
