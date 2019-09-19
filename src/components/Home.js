45043e1835f64e8ab899a03ef08fba40f53bf35b
Dom Gaulton <d.gaulton@catchdigital.com>
2019-09-19 12:30:30 +0100

    BZ-9 Home routing

---
+++ src/components/Home.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { firestore } from "../base";
+import '../styles/App.css';
+
+function Home(props) {
+
+  const [roomName, set_roomName] = useState('')
+
+  const handleInputChange = e => {
+    set_roomName(e.currentTarget.value)
+  }
+
+  const handleRoomChecker = e => {
+    e.preventDefault();
+    // console.log(roomName);
+    const taverns = firestore.collection("taverns");
+    taverns.where("name", "==", roomName)
+    .get()
+    .then(function(querySnapshot) {
+      console.log(querySnapshot)
+      if (!querySnapshot.empty) {
+      querySnapshot.forEach(function(doc) {
+          // doc.data() is never undefined for query doc snapshots
+          console.log(doc.id);
+            console.log('redriect');
+            props.history.push(`/tavern/${doc.id}`)
+            // return (
+            //   <Router>
+            //     <Route path="/tavern/" component={Tavern} />
+            //   </Router>
+            // )
+
+      });
+      }
+
+      if (!querySnapshot.empty) {
+        console.log('redriect');
+        // props.history.push(`/target`)
+        // return (
+        //   <Router>
+        //     <Route path="/tavern/" component={Tavern} />
+        //   </Router>
+        // )
+      }
+
+        // querySnapshot.forEach(function(doc) {
+        //     // doc.data() is never undefined for query doc snapshots
+        //     console.log(doc.id, " => ", doc.data());
+        // });
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+  }
+
+  return (
+    <div className="App">
+      <h1>Login</h1>
+       <form onSubmit={e => handleRoomChecker(e)}>
+        <input type='text' placeholder='Enter Tavern Name' onChange={e => handleInputChange(e)}/>
+      </form>
+    </div>
+  );
+}
+
+export default Home;
