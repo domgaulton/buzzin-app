1447f45531c978e93a67579e68265793c4db5066
Dom Gaulton <d.gaulton@catchdigital.com>
2019-10-01 16:52:56 +0100

    Update env settings and working but not tested

--- src/base.js
+++ src/base.js
@@ -1,8 +1,9 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
@@ -14,9 +15,10 @@ const firebaseConfig = {
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 const firestore = firebaseApp.firestore();
+const auth = firebaseApp.auth();
 
 // This is a named export
-export { firestore };
+export { firestore, auth };
 
 // this is a default export
 export default firebaseApp;
