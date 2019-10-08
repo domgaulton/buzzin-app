03026af8397df27f5f118b439c42d47870e2b9a9
Dom Gaulton <d.gaulton@catchdigital.com>
2019-10-08 13:12:44 +0100

    #57 moved context providers inside the router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import FirebaseUserProvider from "./context/ContextFirebaseUserProvider";
-import FirebaseTavernProvider from "./context/ContextFirebaseTavernProvider";
-import MessageProvider from "./context/ContextMessageProvider";
 import AppRouter from './router/AppRouter';
 
 function App() {
   return (
-    <MessageProvider>
-      <FirebaseUserProvider>
-        <FirebaseTavernProvider>
-          <div className="buzzin-app">
-            <AppRouter />
-          </div>
-        </FirebaseTavernProvider>
-      </FirebaseUserProvider>
-    </MessageProvider>
+    <div className="buzzin-app">
+      <AppRouter />
+    </div>
   );
 }
 
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import FirebaseUserProvider from "../context/ContextFirebaseUserProvider";
+import FirebaseTavernProvider from "../context/ContextFirebaseTavernProvider";
+import MessageProvider from "../context/ContextMessageProvider";
 import Index from "../components/Index";
 import Login from '../components/Auth/Login';
 import Logout from '../components/Auth/Logout';
@@ -16,18 +19,24 @@ import MessageBanner from '../components/General/MessageBanner';
 function AppRouter() {
   return (
     <Router>
-      <MessageBanner />
-      <Route path="/" component={Index} />
-      <Route path="/login" exact component={Login} />
-      <Route path="/logout" exact component={Logout} />
-      <Route path="/user/:userId" component={User} />
-      <Route path="/add-friend" exact component={AddFriend} />
-      <Route path="/settings" component={Settings} />
-      <Route path="/tavern" component={TavernHome} />
-      <Route path="/find-tavern" exact component={FindTavern} />
-      <Route path="/create-tavern" exact component={CreateNewTavern} />
-      <Route path="/tavern/:tavernId" component={Room} />
-      <Navigation />
+      <MessageProvider>
+        <FirebaseUserProvider>
+          <FirebaseTavernProvider>
+            <MessageBanner />
+            <Route path="/" component={Index} />
+            <Route path="/login" exact component={Login} />
+            <Route path="/logout" exact component={Logout} />
+            <Route path="/user/:userId" component={User} />
+            <Route path="/add-friend" exact component={AddFriend} />
+            <Route path="/settings" component={Settings} />
+            <Route path="/tavern" component={TavernHome} />
+            <Route path="/find-tavern" exact component={FindTavern} />
+            <Route path="/create-tavern" exact component={CreateNewTavern} />
+            <Route path="/tavern/:tavernId" component={Room} />
+            <Navigation />
+          </FirebaseTavernProvider>
+        </FirebaseUserProvider>
+      </MessageProvider>
     </Router>
   );
 }
