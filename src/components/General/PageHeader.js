53ab3679449ba9d35f7f7064352a4e4f9e273419
Dom Gaulton <d.gaulton@catchdigital.com>
2019-10-11 19:09:33 +0100

    #62 #66 update a few tickets

--- src/components/General/PageHeader.js
+++ src/components/General/PageHeader.js
@@ -7,7 +7,7 @@ class PageHeader extends Component {
         <h1 className='page-header__title'>{this.props.title}</h1>
         {this.props.settings ?
         (
-          <button onClick={() => this.props.settings()}>
+          <button className='page-header__settings' onClick={() => this.props.settings()}>
             <i className="material-icons">
               delete_forever
             </i>
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import FirebaseUserProvider from "../context/ContextFirebaseUserProvider";
 import FirebaseTavernProvider from "../context/ContextFirebaseTavernProvider";
 import MessageProvider from "../context/ContextMessageProvider";
