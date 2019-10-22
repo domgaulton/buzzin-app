e34dc83d9388d2c340eb76192a6f20ad2948d165
Dom Gaulton <d.gaulton@catchdigital.com>
2019-10-22 09:50:21 +0100

    WIP #72 push notficiations

--- src/index.js
+++ src/index.js
@@ -10,3 +10,37 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+Notification.requestPermission(function(status) {
+  console.log('Notification permission status:', status);
+});
+
+displayNotification();
+
+function displayNotification() {
+  if (Notification.permission === 'granted') {
+    navigator.serviceWorker.getRegistration()
+    .then(reg => {
+      if (reg) {
+        var options = {
+          body: 'Here is a notification body!',
+          icon: 'images/example.png',
+          vibrate: [100, 50, 100],
+          data: {
+            dateOfArrival: Date.now(),
+            primaryKey: 1
+          },
+          actions: [
+            {action: 'explore', title: 'Explore this new world',
+              icon: 'images/checkmark.png'},
+            {action: 'close', title: 'Close notification',
+              icon: 'images/xmark.png'},
+          ]
+        };
+        reg.showNotification('Hello world!', options);
+      } else {
+        console.error('No response from getRegistration')
+      }
+    })
+  }
+}
